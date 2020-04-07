ed1203854c887af91c7ffa776d71029a9c6ab81c
Pete Salamat <psalamat91@rouse.com>
2020-04-07 15:51:15 +0800

    Installed Blazorise nugets

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorApp1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
+
+            builder.Services.AddBaseAddressHttpClient();
+
+            builder.RootComponents.Add<App>("app");
+
+            var host = builder.Build();
+
+            host.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
+            await host.RunAsync();
+        }
     }
 }
